4ec8df9ad637951e2605c6b300361d27fce231cc
kikikieieiei <maxpuall@gmail.com>
2024-10-31 00:14:33 +0700

    Create my projects component

---
+++ app/components/My-Project/my-project.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import './my-project.css';
+import { FaFolder } from "react-icons/fa";
+import { FiMoreVertical } from "react-icons/fi";
+
+const MyProject = () => {
+  const projects = [
+    { id: 1, name: 'Project A' },
+    { id: 2, name: 'Project B' },
+    { id: 3, name: 'Project C' },
+    { id: 4, name: 'Project D' },
+    { id: 5, name: 'Project E' },
+    { id: 6, name: 'Project F' },
+    { id: 7, name: 'Project G' },
+    { id: 8, name: 'Project H' },
+  ];
+
+  const recentProjects = projects.slice(0, 3);
+
+  const itemsPerPage = 3;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const currentProjects = projects.slice(startIndex, startIndex + itemsPerPage);
+
+  const totalPages = Math.ceil(projects.length / itemsPerPage);
+
+  const handlePrevious = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const handleNext = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+
+  return (
+    <div>
+      <h2>Recent Projects</h2>
+      <div className='recentContainer'>
+        {recentProjects.map((project) => (
+          <div className='folderIcon' key={project.id}>
+            <div className='iconWithText'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="300px" height="300px" viewBox="0 0 48 48">
+                <path fill="#9947B5" d="M40 12H22l-4-4H8c-2.2 0-4 1.8-4 4v8h40v-4c0-2.2-1.8-4-4-4" />
+                <path fill="#BA5CDB" d="M40 12H8c-2.2 0-4 1.8-4 4v20c0 2.2 1.8 4 4 4h32c2.2 0 4-1.8 4-4V16c0-2.2-1.8-4-4-4" />
+              </svg>
+              <span className='iconText'>{project.name}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className='allProjects'>
+        <h2>All Projects</h2>
+        {/* Pagination controls */}
+        <div className='pagination'>
+          <button onClick={handlePrevious} disabled={currentPage === 1}>
+            <p>&lt;</p>
+          </button>
+          {currentPage} / {totalPages}
+          <button onClick={handleNext} disabled={currentPage === totalPages}>
+            <p>&gt;</p>
+          </button>
+        </div>
+      </div>
+      {currentProjects.map((project) => (
+        <div className='projectContainer' key={project.id}>
+          <div>
+            <FaFolder />
+            <p>{project.name}</p>
+          </div>
+          <FiMoreVertical />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MyProject;
